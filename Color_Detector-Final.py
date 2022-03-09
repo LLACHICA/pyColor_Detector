eb5738c7e64202fac39f0d0e0269cb81e01463a5
LLACHICA <llachica.578@gmail.com>
2022-03-09 16:26:47 -0500

    test update

--- Color_Detector-Final.py
+++ Color_Detector-Final.py
@@ -1,8 +1,8 @@
 import cv2
-#import numpy as np
 import pandas as pd
 from gooey import Gooey, GooeyParser
 
+# Setup Gooey as a Front-End interface for the Color-Detection Apps
 @Gooey(program_name="AML-1214 Color Detector",
        terminal_font_size=10,
        terminal_font_color='red',
@@ -15,13 +15,14 @@ from gooey import Gooey, GooeyParser
            'name': 'AML-1214 Color Detector',
            'description': 'AML-1214 Color Detector - TERM Project',
            'developer': '\nLouie Lachica\nAileen Dino\nVibhav Sahni\nSri Aravind Devavarapu'
-       }]
-       }])
+                                                }]
+            }])
+# Using GooeyParser to parse the path of the Image File selected by the user
 def parse_args():
     parser = GooeyParser(description="Detect Color on the Selected Image File")
     parser.add_argument('Image_Path', widget="FileChooser")
-
     return parser.parse_args()
+
 args = parse_args()
 img_path=(args.Image_Path)
 print(f'Processing Image.....{img_path}\n\nPress ESC to close the Image Window')
@@ -29,43 +30,45 @@ print(f'Processing Image.....{img_path}\n\nPress ESC to close the Image Window')
 # Opening the image file from file chooser
 img = cv2.imread(img_path)
 
-# Global Variable for click,  R.G.B values and x,y posistion
+# Setting Global Variable for click,  R.G.B values and x,y posistion
 clicked = False
 r = g = b = xpos = ypos = 0
 
-# Reading color.csv file with pandas and giving names to each column
+# Open color.csv file and setting column names using pandas
 index = ["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
 
-# function to calculate minimum distance from all colors and get the most matching color
-def getColorName(R, G, B):
-    minimum = 10000
+# Function to get the matching color and calculate the distance from all color
+def get_ColorN(R, G, B):
+    min = 10000
     for i in range(len(csv)):
         d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
-        if (d <= minimum):
-            minimum = d
+        if (d <= min):
+            min = d
             cname = csv.loc[i, "color_name"]
     return cname
 
 
-# this function will get x,y coordinates of mouse cursor when double clicked.
-def draw_function(event, x, y, flags, param):
+# Function to get the rgb values of the pixel when we double clicked and the (x,y) coordinates of the mouse position
+def Func_Draw(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         global b, g, r, xpos, ypos, clicked
         clicked = True
         xpos = x
         ypos = y
         b, g, r = img[y, x]
+        print(f'Clicked R.G.B values {img[y, x]}')
         b = int(b)
         g = int(g)
         r = int(r)
-
-
+# Open the image in  the new window
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.setMouseCallback('image', draw_function)
 
-while (1):
+# cv2 capture mouse click event
+cv2.setMouseCallback('image', Func_Draw)
+
+while True:
 
     cv2.imshow("image", img)
     if (clicked):
@@ -74,7 +77,7 @@ while (1):
         cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
         # Creating text string to display( Color name and RGB values )
-        text = getColorName(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+        text = get_ColorN(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
 
         # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
         cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
@@ -86,7 +89,7 @@ while (1):
         clicked = False
 
     # Break the loop when user hits 'esc' key
-    if cv2.waitKey(20) & 0xFF == 27:
+    if cv2.waitKey(10) & 0xFF == 27:
         break
 
 cv2.destroyAllWindows()
